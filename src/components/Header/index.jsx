c31e5591c4f719133fd9a5de0ac5106010241c09
Linn <linn.johansson2@edu.cmeducations.se>
2023-11-28 13:37:03 +0100

    Create Header and run test, test passed

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+const Header = ({ title}) => {
+    return (
+        <div className="header">
+            <h1>{title}</h1>
+            <h2>Subtitle texty</h2>
+        </div>
+    )
+}
+
+export default Header;
