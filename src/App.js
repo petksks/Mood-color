48665fe6bdf230bc18757278cd83144feb8623a1
Linn <linn.johansson2@edu.cmeducations.se>
2023-12-04 13:02:57 +0100

    Add local storge and a rednder-logic for naming My Colors + number

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -8,23 +8,55 @@ import AddColor from './components/AddColor';
 
 function App() {
 
-  const [colors, setColors] = useState(Color);
-  const [idCounter, setIdCounter] = useState(1); 
+  //const [colors, setColors] = useState(Color);
+
+  const [colors, setColors] = useState(() => {
+    const storedColors = localStorage.getItem('colors');
+    return storedColors ? JSON.parse(storedColors) : Color;
+  });
+
+  //const [idCounter, setIdCounter] = useState(1); 
   
+  useEffect(() => {
+    localStorage.setItem('colors', JSON.stringify(colors));
+  }, [colors]);
+
   const handleColorArrayChange = (newColor) => {
+    const storedColors = localStorage.getItem('colors');
+    const parsedColors = storedColors ? JSON.parse(storedColors) : Color;
+  
+    const maxNumber = parsedColors.reduce((max, color) => {
+      const match = color.title.match(/My Color (\d+)/);
+      if (match) {
+        return Math.max(max, parseInt(match[1]));
+      }
+      return max;
+    }, 0);
+  
+    const newName = `My Color ${maxNumber + 1}`;
+  
     setColors((prevColors) => {
       const updatedColors = [
         ...prevColors,
-        { id: 'My Color ' + idCounter, color: newColor.hex, titel: 'My Color ' + idCounter}
+        { id: newName, color: newColor.hex, title: newName }
       ];
-        return updatedColors;
+      return updatedColors;
     }); 
-    setIdCounter((prevIdCounter) => prevIdCounter + 1);
   };
 
   const handleDeleteColor = (id) => {
     const updatedColors = colors.filter((color) => color.id !== id);
     setColors(updatedColors);
+  
+    const reOrderedColors = updatedColors.map((color, index) => {
+      const newName = `My Color ${index + 1}`;
+      return {
+        ...color,
+        id: newName,
+        title: newName
+      };
+    });
+    setColors(reOrderedColors);
   };
 
   return (
